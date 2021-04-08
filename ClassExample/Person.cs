401a90d5cfbb89c2a366d73d505f2c36afcdfca5
beerinder-git <beer.singh@visioncollege.ac.nz>
2021-04-09 09:58:30 +1200

    Add project files.

---
+++ ClassExample/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExample
+{
+    class Person
+    {
+        // Member Variables
+        // method s
+
+        private  string name { get;  set; }
+        private int age { get; set; }
+
+        // Methods
+
+        public void SetMember (string setName, int setAge)
+        {
+            name = setName;
+            age = setAge;
+        }
+
+        public void GetMember(out string getName, out int getAge)
+        {
+            getName =name;
+            getAge = age;
+        }
+
+
+    }
+
+    
+}
---
+++ ClassExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassExample
+{
+    class Program
+    {
+        public static Person obj = new Person();
+        public static Person obj1 = new Person();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Please enter name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter age ");
+            string age = Console.ReadLine();
+
+            int newAge = Convert.ToInt32(age);
+
+            obj.SetMember(name, newAge);
+
+            obj.GetMember(out name, out newAge);
+            Console.WriteLine("Name: " + name + " Age " + newAge);
+
+            Console.WriteLine("Please enter name for Obj1 ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter age Obj1");
+            age = Console.ReadLine();
+
+            newAge = Convert.ToInt32(age);
+
+            obj1.SetMember(name, newAge);
+            obj1.GetMember(out name, out newAge);
+            Console.WriteLine("Name: " + name + " Age " + newAge);
+        }
+    }
+}
